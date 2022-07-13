bd33d3b04bd3ed8e81d84e575db3e3feb7946b43
thopz <clumen600@gmail.com>
2022-07-13 19:28:31 +0800

    schema

---
+++ ecommerce/schemas/product.js
@@ -0,0 +1,35 @@
+export default {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'array', // of image objects
+      of: [{ type: 'image' }],
+      options: { hotspot: true },
+    },
+    {
+      name: 'name',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: { source: 'name', maxLength: 96 },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    },
+    {
+      name: 'details',
+      title: 'Details',
+      type: 'string',
+    },
+  ],
+};
