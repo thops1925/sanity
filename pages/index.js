d62370ebf00f32ef8bfcf848b9f65363cce36750
thopz <clumen600@gmail.com>
2022-07-13 22:14:04 +0800

    add style css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-function index() {
-  return <div>index</div>;
-}
+const Home = () => {
+  return (
+    <>
+      HeroBanner
+      <div>
+        <h2>GFX System </h2>
+        <p>
+          Large format mirrorless digital camera system that achieves the
+          world’s highest level of image quality.
+        </p>
+      </div>
+      <div>
+        {[
+          'product1',
+          'product2',
+          'product3',
+          'product4',
+          'product5',
+          'product6',
+        ].map((product) => product)}
+      </div>
+      Footer
+    </>
+  );
+};
 
-export default index;
+export default Home;
