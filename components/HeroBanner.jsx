6495f0611e4f18645baf3cfa29b182a6a6fef846
thopz <clumen600@gmail.com>
2022-07-17 14:05:11 +0800

    edit css

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -15,20 +15,18 @@ const HeroBanner = ({
 }) => {
   return (
     <div className="hero-banner-container">
-      <div>
-        <p className="beats-solo">{smallText}</p>
-        <h3>{midText}</h3>
-        <h1>{largeText1}</h1>
+      <p className="beats-solo">{smallText}</p>
+      <h3>{midText}</h3>
+      <h1>{largeText1}</h1>
 
-        <img src={urlFor(image)} alt="camera" className="hero-banner-image" />
-        <div>
-          {/* <Link href={`/product/${product}`}>
+      <img src={urlFor(image)} alt="camera" className="hero-banner-image" />
+      <div>
+        {/* <Link href={`/product/${product}`}>
             {/* <button type="button">{buttonText}</button> */}
-          {/* </Link> */}
-          <div className="desc">
-            <h5>Description</h5>
-            <p>{desc}</p>
-          </div>
+        {/* </Link> */}
+        <div className="desc">
+          <h5>Description</h5>
+          <p>{desc}</p>
         </div>
       </div>
     </div>
