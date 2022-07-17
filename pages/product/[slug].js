fa0ee736f875e45e15760123b513ec69e41c6ceb
ThopsD <74445064+thops1925@users.noreply.github.com>
2022-07-18 00:19:59 +0800

    add button to add to cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,16 +1,63 @@
 import React from 'react';
 import { client, urlFor } from '../../lib/client';
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineStar,
+  AiFillStar,
+} from 'react-icons/ai';
 
 const ProductDetails = ({ product, products }) => {
-  const { title, details, price, image } = product;
+  const { name, details, price, image } = product;
   return (
-    <div className="product-detail-container">
-      <div>
-        <div className="image-container">
-          <img src={urlFor(image && image[0])} />
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src={urlFor(image && image[0])} />
+          </div>
+          {/* <div className="small-images-container">
+            {image?.map((image, index) => (
+              <img key={index} src={urlFor(image)} onMouseEnter="" />
+            ))}
+          </div> */}
+          <div className="product-detail-desc">
+            <h1>{name}</h1>
+            <div className="reviews">
+              <div>
+                <AiFillStar />
+                <AiFillStar />
+                <AiFillStar />
+                <AiOutlineStar />
+              </div>
+              <p>(10)</p>
+            </div>
+            <h4>Details:</h4>
+            <p>{details}</p>
+            <p className="price">₱{price}</p>
+            <div className="quantity">
+              <h3>Quantity:</h3>
+              <p className="quantity-desc">
+                <span className="minus">
+                  <AiOutlineMinus />
+                </span>
+                <span className="num">0</span>
+                <span className="plus">
+                  <AiOutlinePlus />
+                </span>
+              </p>
+            </div>
+            <div className="buttons">
+              <button className="add-to-cart" type="button" onClick="">
+                Add to Cart
+              </button>
+              <button className="buy-now" type="button" onClick="">
+                Buy Now
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-      ProductDetails
     </div>
   );
 };
