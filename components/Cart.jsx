517d3f96f0ece37f6525a2b4d8f539afcedf4a4e
ThopsD <74445064+thops1925@users.noreply.github.com>
2022-07-28 03:13:14 +0800

    Card fix

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -14,7 +14,7 @@ import { urlFor } from '../lib/client';
 const Cart = () => {
   const cartRef = useRef();
   const { setShowCart, cart, totalQuantity, totalPrice } = useStateContext();
-
+  console.log(totalQuantity);
   return (
     <div className="cart-wrapper" ref={cartRef}>
       <div className="cart-container">
@@ -25,8 +25,51 @@ const Cart = () => {
         >
           <AiOutlineLeft />
           <span className="heading">Your Cart</span>
-          <span className="cart-num-items"> {totalQuantity}</span>
+          <span className="cart-num-items">({totalQuantity}item)</span>
         </button>
+        {cart.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping />
+            <h3> Your Shopping bag is Empty</h3>
+            <Link href="/">
+              <button
+                type="button"
+                onClick={() => setShowCart(false)}
+                className="btn"
+              >
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
+        <div className="product-container">
+          {cart.length >= 1 &&
+            cart.map((product) => (
+              <div className="product" key={product._id}>
+                <img
+                  src={urlFor(product?.image[0])}
+                  className="cart-product-image"
+                />
+                <div className="item-desc">
+                  <div className="flex top">
+                    <h5>{product.name}</h5>
+                    <h4>{product.price}</h4>
+                  </div>
+                  <div className="flex bottom">
+                    <div className="quantity-desc">
+                      <span className="minus">
+                        <AiOutlineMinus />
+                      </span>
+                      <span className="num"></span>
+                      <span className="plus">
+                        <AiOutlinePlus />
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
